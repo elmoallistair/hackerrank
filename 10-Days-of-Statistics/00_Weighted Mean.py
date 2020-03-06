74261465380ed2878f1179a87051a69de7dc1032
Elmo Allistair <55618192+elmoallistair@users.noreply.github.com>
2020-03-06 07:28:17 +0700

    Update 00_Weighted Mean.py

--- 10-Days-of-Statistics/00_Weighted Mean.py
+++ 10-Days-of-Statistics/00_Weighted Mean.py
@@ -3,8 +3,8 @@
 * Resource: https://www.hackerrank.com/challenges/s10-weighted-mean/tutorial
 *
 * Task:
-* Given an array, X, of N integers and an array, W, representing the respective weights of X's elements, calculate and print the weighted mean of X's elements.
-* Your answer should be rounded to a scale of 1 decimal place (i.e., 12.3 format).
+*   Given an array, X, of N integers and an array, W, representing the respective weights of X's elements, calculate and print the weighted mean of X's elements.
+*   Your answer should be rounded to a scale of 1 decimal place (i.e., 12.3 format).
 '''
 
 # Enter your code here. Read input from STDIN. Print output to STDOUT
