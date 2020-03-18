7674fc1cc01a48b48de9f6c93772929c8a711e5d
Elmo Allistair <55618192+elmoallistair@users.noreply.github.com>
2020-03-18 10:32:36 +0700

    Minor changes

--- 10-Days-of-Javascript/Day00_Hello-World.js
+++ 10-Days-of-Javascript/Day00_Hello-World.js
@@ -1,12 +1,12 @@
 /*
-* Task
-*
-* A greeting function is provided for you in the editor below. It has one parameter, 'parameterVariable'.
-* Perform the following tasks to complete this challenge:
-*
-*   1. Use console.log() to print Hello, World! on a new line in the console, which is also known as stdout or standard output. The code for this portion of the task is already provided in the editor.
-*   2. Use console.log() to print the contents of 'parameterVariable' (i.e., the argument passed to main).
-*/
+ * Task
+ *
+ * A greeting function is provided for you in the editor below. It has one parameter, 'parameterVariable'.
+ * Perform the following tasks to complete this challenge:
+ *
+ *   1. Use console.log() to print Hello, World! on a new line in the console, which is also known as stdout or standard output. The code for this portion of the task is already provided in the editor.
+ *   2. Use console.log() to print the contents of 'parameterVariable' (i.e., the argument passed to main).
+ */
 
 /* ============================================================================ */
 
