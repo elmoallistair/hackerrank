7aeabf235ed42aadc42dbac9ed76f601e768db87
Elmo Allistair <55618192+elmoallistair@users.noreply.github.com>
2020-03-25 01:10:04 +0700

    Update and rename 10-Days-of-Javascript/Day01_Arithmetic-Operators to 10-Days-of-Javascript/Day 01 Arithmetic Operators/code.js

--- 10-Days-of-Javascript/Day 01 Arithmetic Operators/code.js
+++ 10-Days-of-Javascript/Day 01 Arithmetic Operators/code.js
@@ -1,16 +1,4 @@
 // Written: 18-Mar-2020
-/*
- * Task
- *
- * Complete the following functions in the editor below:
- *
- *   1. getArea(length, width): Calculate and return the area of a rectangle having sides and $width.
- *   2. getPerimeter(length, width): Calculate and return the perimeter of a rectangle having sides $length and $width.
- *
- * The values returned by these functions are printed to stdout by locked stub code in the editor.
- */
-
- /* ============================================================================ */
 
  'use strict';
 
