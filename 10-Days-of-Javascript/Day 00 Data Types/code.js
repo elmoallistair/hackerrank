7a92e5617132193611e46957e959ad41fcb717f6
Elmo Allistair <55618192+elmoallistair@users.noreply.github.com>
2020-03-25 00:21:08 +0700

    Update and rename 10-Days-of-Javascript/Day00_Data-Types.js to 10-Days-of-Javascript/Day 00 Data Types/code.js

--- 10-Days-of-Javascript/Day 00 Data Types/code.js
+++ 10-Days-of-Javascript/Day 00 Data Types/code.js
@@ -1,16 +1,4 @@
 // Written: 20-Mar-2020
-/*
- * Task
- *
- * Variables named $firstInteger, $firstDecimal, and $firstString are declared for you in the editor below.
- * You must use the + operator to perform the following sequence of operations:
- *
- *   1. Convert $secondInteger to an integer (Number type), then sum it with $firstInteger and print the result on a new line using console.log.
- *   2. Convert $secondDecimal to a floating-point number (Number type), then sum it with $firstDecimal and print the result on a new line using console.log.
- *   3. Print the concatenation of firstString and secondString on a new line using console.log. Note that $firstString must be printed first.
- */
-
-/* ============================================================================ */
 
 'use strict';
 
