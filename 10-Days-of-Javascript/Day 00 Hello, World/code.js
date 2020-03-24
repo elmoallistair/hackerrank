f7c32afd1fb625b40099f4550c5858dc85a73770
Elmo Allistair <55618192+elmoallistair@users.noreply.github.com>
2020-03-24 23:52:01 +0700

    Update and rename 10-Days-of-Javascript/Day00_Hello-World.js to 10-Days-of-Javascript/Day 00 Hello, World/code.js

--- 10-Days-of-Javascript/Day 00 Hello, World/code.js
+++ 10-Days-of-Javascript/Day 00 Hello, World/code.js
@@ -1,15 +1,4 @@
 // Written: 18-Mar-2020
-/*
- * Task
- *
- * A greeting function is provided for you in the editor below. It has one parameter, 'parameterVariable'.
- * Perform the following tasks to complete this challenge:
- *
- *   1. Use console.log() to print Hello, World! on a new line in the console, which is also known as stdout or standard output. The code for this portion of the task is already provided in the editor.
- *   2. Use console.log() to print the contents of 'parameterVariable' (i.e., the argument passed to main).
- */
-
-/* ============================================================================ */
 
 'use strict';
 
