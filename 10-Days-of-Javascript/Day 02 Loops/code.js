5af34ad2e95ce80992069e7568b62000abaade57
Elmo Allistair <55618192+elmoallistair@users.noreply.github.com>
2020-03-25 01:38:43 +0700

    Update and rename 10-Days-of-Javascript/Day02_Loops.js to 10-Days-of-Javascript/Day 02 Loops/code.js

--- 10-Days-of-Javascript/Day 02 Loops/code.js
+++ 10-Days-of-Javascript/Day 02 Loops/code.js
@@ -1,14 +1,4 @@
 // Written: 20-Mar-2020
-/*
- * Task
- * 
- * Complete the vowelsAndConsonants function in the editor below. It has one parameter, a string, $s, consisting of lowercase English alphabetic letters (i.e., a through z). The function must do the following:
- * 
- *  1. First, print each vowel in $s on a new line. The English vowels are a, e, i, o, and u, and each vowel must be printed in the same order as it appeared in $s.
- *  2. Second, print each consonant (i.e., non-vowel) in $s on a new line in the same order as it appeared in $s.
- */
-
-/* ============================================================================ */
 
 'use strict';
 
