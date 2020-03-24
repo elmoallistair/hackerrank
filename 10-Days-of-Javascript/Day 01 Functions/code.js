5824eca2c23ae4fd1bd53ad0954a921a696b9ee1
Elmo Allistair <55618192+elmoallistair@users.noreply.github.com>
2020-03-25 01:33:45 +0700

    Update and rename 10-Days-of-Javascript/Day01_Functions.js to 10-Days-of-Javascript/Day 01 Functions/code.js

--- 10-Days-of-Javascript/Day 01 Functions/code.js
+++ 10-Days-of-Javascript/Day 01 Functions/code.js
@@ -1,12 +1,4 @@
 // Written: 20-Mar-2020
-/*
- * Task
- *
- * Implement a function named factorial that has one parameter: an integer, n. 
- * It must return the value of n!(i.e.,n factorial).
- */
-
-/* ============================================================================ */
 
 'use strict';
 
