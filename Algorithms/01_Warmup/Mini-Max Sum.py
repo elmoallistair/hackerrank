1d88aa802b0c16567b9e4e05b6b72b5418cbd9e3
Elmo Allistair <55618192+elmoallistair@users.noreply.github.com>
2020-03-26 16:05:06 +0700

    Create Mini-Max Sum.py

---
+++ Algorithms/01_Warmup/Mini-Max Sum.py
@@ -0,0 +1,21 @@
+# Written: 31-Dec-2019
+# Link: https://www.hackerrank.com/challenges/mini-max-sum/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the miniMaxSum function below.
+def miniMaxSum(arr):
+    arr = sorted(arr)
+    min = arr[0:len(arr)-1]
+    max = arr[1::]
+    print(sum(min), sum(max))
+
+if __name__ == '__main__':
+    arr = list(map(int, input().rstrip().split()))
+    miniMaxSum(arr)
