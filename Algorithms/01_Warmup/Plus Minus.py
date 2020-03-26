fcdb5f44b2245327e457a493b0f280d2bffe79bb
Elmo Allistair <55618192+elmoallistair@users.noreply.github.com>
2020-03-26 16:00:50 +0700

    Create Plus Minus.py

---
+++ Algorithms/01_Warmup/Plus Minus.py
@@ -0,0 +1,36 @@
+# Link: https://www.hackerrank.com/challenges/plus-minus/problem
+# Written: 15-Dec-2019
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the plusMinus function below.
+def plusMinus(arr):
+    count_pos = count_neg = count_zero = 0
+    for num in arr:
+        if num > 0:
+            count_pos += 1
+        elif num < 0:
+            count_neg += 1
+        else:
+            count_zero += 1
+
+    print(count_pos/n)
+    print(count_neg/n)
+    print(count_zero/n)
+
+    # Shorter Code:
+    # print(len([num for num in arr if num > 0])/n)
+    # print(len([num for num in arr if num < 0])/n)
+    # print(len([num for num in arr if num == 0])/n)
+
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().rstrip().split()))
+    plusMinus(arr)
