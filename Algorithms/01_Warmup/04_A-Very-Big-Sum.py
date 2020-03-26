a304c5c34d571767bf1b6a2c03d048df1ff44fa3
Elmo Allistair <55618192+elmoallistair@users.noreply.github.com>
2020-03-26 15:57:20 +0700

    Update

--- Algorithms/01_Warmup/04_A-Very-Big-Sum.py
+++ Algorithms/01_Warmup/04_A-Very-Big-Sum.py
@@ -1,14 +1,5 @@
 # Written: 12-Dec-2019
-'''
-* Calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
-*
-* Function Description
-*   Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
-*   aVeryBigSum has the following parameter(s):
-*     - ar: an array of integers .
-'''
-
-# ===============================================
+# https://www.hackerrank.com/challenges/a-very-big-sum/problem
 
 #!/bin/python3
 
