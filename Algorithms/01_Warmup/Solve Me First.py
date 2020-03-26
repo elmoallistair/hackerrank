41b746661956bea680b488266660a4937aa81f5b
Elmo Allistair <55618192+elmoallistair@users.noreply.github.com>
2020-03-26 15:43:45 +0700

    Update

---
+++ Algorithms/01_Warmup/Solve Me First.py
@@ -0,0 +1,9 @@
+# Written: 11-Dec-2019
+# https://www.hackerrank.com/challenges/solve-me-first
+
+def solveMeFirst(a,b):
+	return a + b
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
