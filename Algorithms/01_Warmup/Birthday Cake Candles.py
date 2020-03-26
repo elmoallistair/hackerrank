5bc5d1a366ef05c046948862900e6abcfd6087f9
Elmo Allistair <55618192+elmoallistair@users.noreply.github.com>
2020-03-26 16:06:43 +0700

    Create Birthday Cake Candles.py

---
+++ Algorithms/01_Warmup/Birthday Cake Candles.py
@@ -0,0 +1,27 @@
+# Written: 01-Jan-2020
+# https://www.hackerrank.com/challenges/birthday-cake-candles/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the birthdayCakeCandles function below.
+def birthdayCakeCandles(ar):
+    tallest = max(ar)
+    res = len([x for x in ar if x == tallest])
+    return res
+
+    # Shorter Code:
+    # return ar.count(max(ar))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    ar_count = int(input())
+    ar = list(map(int, input().rstrip().split()))
+    result = birthdayCakeCandles(ar)
+    fptr.write(str(result) + '\n')
+    fptr.close()
