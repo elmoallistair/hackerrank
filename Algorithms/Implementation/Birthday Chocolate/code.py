2f04e06af034efd6fc30c8dc6d6e7b5309956cc2
Elmo Allistair <55618192+elmoallistair@users.noreply.github.com>
2020-03-27 20:42:12 +0700

    Create code.py

---
+++ Algorithms/Implementation/Birthday Chocolate/code.py
@@ -0,0 +1,32 @@
+# Written: 03-Jan-2020
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the birthday function below.
+def birthday(s, d, m):
+    pieces = 0
+    for i in range(len(s)):
+        n = 0
+        count = 0
+        for j in range(n,m):
+            if i+j == len(s):
+                break
+            count += s[i+j]
+        if count == d:
+            pieces += 1
+    return pieces
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    s = list(map(int, input().rstrip().split()))
+    dm = input().rstrip().split()
+    d = int(dm[0])
+    m = int(dm[1])
+    result = birthday(s, d, m)
+    print(result)
