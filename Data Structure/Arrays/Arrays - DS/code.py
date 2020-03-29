2a960498ff1a33d6aaf4ef69fc2433d0eb9c1d88
Elmo Allistair <55618192+elmoallistair@users.noreply.github.com>
2020-03-29 22:45:08 +0700

    Create code.py

---
+++ Data Structure/Arrays/Arrays - DS/code.py
@@ -0,0 +1,23 @@
+# Written: 29-Mar-2020
+# https://www.hackerrank.com/challenges/arrays-ds/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the reverseArray function below.
+def reverseArray(a):
+    return reversed(a)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    arr_count = int(input())
+    arr = list(map(int, input().rstrip().split()))
+    res = reverseArray(arr)
+    fptr.write(' '.join(map(str, res)))
+    fptr.write('\n')
+    fptr.close()
