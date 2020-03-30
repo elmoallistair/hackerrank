6a15a6ad5b6d672ddedafcdcb89f27f664bce155
Elmo Allistair <55618192+elmoallistair@users.noreply.github.com>
2020-03-31 00:56:21 +0700

    Create Mutations.py

---
+++ Python/Strings/Mutations.py
@@ -0,0 +1,11 @@
+# Written: 07-Jan-2020
+# https://www.hackerrank.com/challenges/python-mutations/problem
+
+def mutate_string(string, position, character):
+    return string[:position] + character + string[position+1:]
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
