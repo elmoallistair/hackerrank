e74a4e548a2ac09c96cbf34490bf51230ba2590d
Elmo Allistair <55618192+elmoallistair@users.noreply.github.com>
2020-04-02 18:27:45 +0700

    Create Capitalize!.py

---
+++ Python/Strings/Capitalize!.py
@@ -0,0 +1,27 @@
+# Written: 21-Jan-2020
+# https://www.hackerrank.com/challenges/capitalize/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(s):
+    new_s = list()
+    for i in range(len(s)):
+        if (i == 0 or s[i-1] == ' '):
+            new_s.append(s[i].upper())
+        else:
+            new_s.append(s[i])
+    return ''.join(new_s)
+    # return ''.join([s[i].upper() if (i == 0 or s[i-1] == ' ') else s[i] for i in range(len(s))])
+
+if __name__ == '__main__':
+    # s = input()
+    s = 'abc def ghi'
+    result = solve(s)
+    print(result)
