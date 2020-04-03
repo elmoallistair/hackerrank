8fb7bdd454236772dff56884180df7cd71070da3
Elmo Allistair <55618192+elmoallistair@users.noreply.github.com>
2020-04-03 22:28:06 +0700

    Create Input and Output.cpp

---
+++ C++/Introduction/Input and Output.cpp
@@ -0,0 +1,18 @@
+// Written: 02-Apr-2020
+// https://www.hackerrank.com/challenges/cpp-input-and-output/problem
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int a,b,c;
+    cin >> a >> b >> c;   
+    cout << a+b+c;
+    return 0;
+}
