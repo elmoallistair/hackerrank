70add6587a2e62c2ce03bc362a1e7f75b87934b4
Elmo Allistair <55618192+elmoallistair@users.noreply.github.com>
2020-04-03 22:27:28 +0700

    Create Say "Hello, World!" With C++.cpp

---
+++ C++/Introduction/Say "Hello, World!" With C++.cpp
@@ -0,0 +1,11 @@
+// Written: 02-Apr-2020
+// https://www.hackerrank.com/challenges/cpp-hello-world/problem
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
