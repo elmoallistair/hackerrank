5f9b4b841f2ef40628ff6c634f891bdfde05fd0b
Elmo Allistair <55618192+elmoallistair@users.noreply.github.com>
2020-04-03 16:19:12 +0700

    Create Triangle Quest.py

---
+++ Python/Math/Triangle Quest.py
@@ -0,0 +1,5 @@
+# Written: 03-Apr-2020
+# https://www.hackerrank.com/challenges/python-quest-1/problem?
+
+for i in range(1,int(input())): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print(int(i*((10**i-1)/9)))
