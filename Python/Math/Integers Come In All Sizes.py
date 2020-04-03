e72a6ee4dff528127a2ed2b3abc6783b090e5b16
Elmo Allistair <55618192+elmoallistair@users.noreply.github.com>
2020-04-03 16:10:24 +0700

    Create Integers Come In All Sizes.py

---
+++ Python/Math/Integers Come In All Sizes.py
@@ -0,0 +1,11 @@
+# Written: 03-Apr-2020
+# https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+print(pow(a,b) + pow(c,d))
