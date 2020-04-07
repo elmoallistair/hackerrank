b707a71696b2d7c0869aaf5f7aeed1f0e11c5bee
Elmo Allistair <55618192+elmoallistair@users.noreply.github.com>
2020-04-07 23:20:12 +0700

    Create code.kt

---
+++ 30 Days of Code/Day 00 Hello, World/code.kt
@@ -0,0 +1,10 @@
+// Written: 07-Apr-2020
+// https://www.hackerrank.com/challenges/30-hello-world/problem
+import java.io.*
+import java.util.*
+
+fun main(args: Array<String>) {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT. */
+    var inputString = readLine()
+    println("Hello, World.\n$inputString")
+}
