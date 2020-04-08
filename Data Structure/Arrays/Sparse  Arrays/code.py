3a880848d10eebf0af1cb888bab3040907ba1a61
Elmo Allistair <55618192+elmoallistair@users.noreply.github.com>
2020-04-08 22:57:23 +0700

    Create code.py

---
+++ Data Structure/Arrays/Sparse  Arrays/code.py
@@ -0,0 +1,39 @@
+# Written: 08-Apr-2020
+# https://www.hackerrank.com/challenges/sparse-arrays/problem?
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the matchingStrings function below.
+def matchingStrings(strings, queries):
+    count = []
+    for i in range(len(queries)):
+        count.append(strings.count(queries[i]))
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    strings_count = int(input())
+    strings = []
+
+    for _ in range(strings_count):
+        strings_item = input()
+        strings.append(strings_item)
+
+    queries_count = int(input())
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(strings, queries)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+    fptr.close()
