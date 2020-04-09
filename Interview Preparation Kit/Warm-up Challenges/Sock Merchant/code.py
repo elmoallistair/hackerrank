9b98ce9a10ad391bb02e6afbd80f07cce3550f11
Elmo Allistair <55618192+elmoallistair@users.noreply.github.com>
2020-04-09 20:23:16 +0700

    Create code.py

---
+++ Interview Preparation Kit/Warm-up Challenges/Sock Merchant/code.py
@@ -0,0 +1,25 @@
+# Written: 09-Apr-2020
+# https://www.hackerrank.com/challenges/sock-merchant/problem?
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+    count = 0
+    for i in set(ar):
+        count += ar.count(i)//2
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    n = int(input())
+    ar = list(map(int, input().rstrip().split()))
+    result = sockMerchant(n, ar)
+    fptr.write(str(result) + '\n')
+    fptr.close()
